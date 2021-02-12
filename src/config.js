d281eb784e3b484a34467456fc6a83d7d9faa891
Austin File <austinfile510@gmail.com>
2021-02-11 23:43:49 -0800

    adding migration scripts

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ module.exports = {
 	API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:3000',
 	DATABASE_URL:
 		process.env.DATABASE_URL ||
-		'postgresql://dunder_mifflin:interactive@localhost/recipe-keeper',
+		'postgresql://dunder_mifflin:interactive@localhost/paper-trail',
 	CORS_METHODS: process.env.REACT_APP_CORS_METHODS || '*',
 	TEST_DATABASE_URL:
 		process.env.DATABASE_URL ||
-		'postgresql://dunder_mifflin:interactive@localhost/recipe-keeper-test',
+		'postgresql://dunder_mifflin:interactive@localhost/paper-trail-test',
 	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '1d',
 };
